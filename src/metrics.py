3b3134c15e1eda2ed1b5ab2b16661f3c826c1123
temakiselevv <artem_kiseleff@mail.ru>
2026-05-19 05:38:03 +0300

    Added new metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,18 @@
-def conversion_rate(df):
-    return df['conversion'].mean()
+import pandas as pd
 
-def uplift(a, b):
-    return None
+
+def click_through_rate(df: pd.DataFrame):
+    return df["of_website_clicks"] / df["of_impressions"] * 100
+
+def conversion_rate(df: pd.DataFrame):
+    return df["of_purchase"] / df["of_website_clicks"] * 100
+
+def cost_per_action(df: pd.DataFrame):
+    return df["spend_usd"] / df["of_purchase"]
+
+def purchase_rate(df: pd.DataFrame):
+    return df['of_purchase'] / df['of_impressions'] * 100
+
+def uplift(control: pd.Series, test: pd.Series):
+    return (test - control) / control * 100
+
